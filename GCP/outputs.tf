3ccdf600df8f25140dfea17eb60e3eb59a6122e4
lalitdeore12@gmail.com <lalitdeore12@gmail.com>
2026-01-19 11:09:30 +0530

    sperate gcp and aws changes

---
+++ GCP/outputs.tf
@@ -0,0 +1,91 @@
+output "deployment_name" {
+  description = "Name of the deployment"
+  value       = local.deployment_name
+}
+
+output "shuffle_frontend_url" {
+  description = "URL to access Shuffle Frontend"
+  value       = "http://${google_compute_instance.swarm_manager[0].network_interface[0].access_config[0].nat_ip}:3001"
+  depends_on  = [null_resource.wait_for_shuffle_deployment]
+}
+
+# HTTPS port 3443 is not exposed externally for security
+# Internal access only via Docker Swarm overlay network
+
+output "opensearch_internal_url" {
+  description = "Internal URL to access OpenSearch (not exposed externally)"
+  value       = "http://${google_compute_instance.swarm_manager[0].network_interface[0].network_ip}:9200"
+}
+
+
+
+output "manager_instances" {
+  description = "List of manager instance details"
+  value = join("\n", [for instance in google_compute_instance.swarm_manager : 
+    "Name: ${instance.name}, Internal IP: ${instance.network_interface[0].network_ip}, External IP: ${instance.network_interface[0].access_config[0].nat_ip}, Zone: ${instance.zone}"
+  ])
+}
+
+
+output "total_nodes" {
+  description = "Total number of nodes in the cluster"
+  value       = local.total_nodes
+}
+
+output "manager_nodes" {
+  description = "Number of manager nodes"
+  value       = local.manager_nodes
+}
+
+
+output "network_name" {
+  description = "Name of the VPC network"
+  value       = google_compute_network.shuffle_network.name
+}
+
+output "subnet_name" {
+  description = "Name of the subnet"
+  value       = google_compute_subnetwork.shuffle_subnet.name
+}
+
+output "nfs_server_ip" {
+  description = "IP address of the NFS server (primary manager)"
+  value       = google_compute_instance.swarm_manager[0].network_interface[0].network_ip
+}
+
+output "swarm_join_command_manager" {
+  description = "Command to join swarm as manager (retrieve from primary manager)"
+  value       = "SSH to primary manager and run: docker swarm join-token manager"
+}
+
+output "swarm_join_command_worker" {
+  description = "Command to join swarm as worker (retrieve from primary manager)"
+  value       = "SSH to primary manager and run: docker swarm join-token worker"
+}
+
+output "post_deployment_instructions" {
+  description = "Instructions after deployment"
+  value       = <<-EOT
+    =================================================================
+    Shuffle Deployment Complete!
+    =================================================================
+    
+    Access URLs:
+    - Frontend (External): http://${google_compute_instance.swarm_manager[0].network_interface[0].access_config[0].nat_ip}:3001
+    - OpenSearch (Internal): http://${google_compute_instance.swarm_manager[0].network_interface[0].network_ip}:9200
+    
+    Note: Only port 3001 is exposed externally for security.
+    All other services are accessible only within the VPC.
+    
+    Cluster Information:
+    - Total Nodes: ${local.total_nodes}
+    - All nodes are Swarm managers
+    
+    Management:
+    - SSH to primary manager: gcloud compute ssh ${google_compute_instance.swarm_manager[0].name} --zone=${google_compute_instance.swarm_manager[0].zone}
+    - View stack: docker stack services shuffle
+    - View logs: docker service logs shuffle_<service-name>
+    
+    =================================================================
+  EOT
+}
